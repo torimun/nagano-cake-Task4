7054957f3658dfa82fd1cf20922a0595210a43b6
ToriMun <132326048+torimun@users.noreply.github.com>
2023-06-28 08:58:55 +0000

    [Add]pubilcのtopページ製作途中

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,9 @@ Rails.application.routes.draw do
     get 'items/index'
     get 'items/show'
   end
-  get 'homes/top'
-  get 'homes/about'
+  namespace :pubilc do
+    get 'homes/top'
+    get 'homes/about'
+  end  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
