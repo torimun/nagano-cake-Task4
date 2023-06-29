192275fba2556248b0b381e7cf470354637210a6
ToriMun <132326048+torimun@users.noreply.github.com>
2023-06-29 04:54:56 +0000

    [Add]bootstrapの追加、pubilc topページの作成途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,12 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
     get 'homes/top'
+    get 'orders/show'
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :sessions, only: [:new, :create, :destroy]
   end
-  namespace :admin do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
+  
   namespace :pubilc do
     get 'orders/new'
     get 'orders/confirm'
