5d10bb6a41b4c4fd686f958047ca7d101886dc76
ToriMun <132326048+torimun@users.noreply.github.com>
2023-07-03 04:54:55 +0000

    [update]pubilc,adminそれぞれのcontrollernの修正

--- config/routes.rb
+++ config/routes.rb
@@ -12,52 +12,22 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
+
   namespace :admins do
     get 'homes/top'
     get 'orders/show'
     resources :customers, only: [:index, :show, :edit, :update]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
-    resources :sessions, only: [:new, :create, :destroy]
   end
 
-  namespace :pubilc do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/done'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :pubilc do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :pubilc do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :pubilc do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/confirm'
-    get 'customers/withdraw'
-  end
-  namespace :pubilc do
-    get 'registrations/new'
-    get 'registrations/create'
-  end
-  namespace :pubilc do
-    get 'items/index'
-    get 'items/show'
-  end
+
   namespace :pubilc do
     get 'homes/top'
     get 'homes/about'
+    resources :orders, only: [:new, :confirm, :done, :create, :index, :show]
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+    resources :customers, only: [:show, :edit, :update, :confirm, :withdraw]
+    resources :items, only: [:index, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
