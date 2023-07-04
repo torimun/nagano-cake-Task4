5e5f651638d796ad8f3ef3ac803dec7f7e7fa32b
ToriMun <132326048+torimun@users.noreply.github.com>
2023-07-04 04:25:59 +0000

    [fix]db:migrateファイル(admins,costomaers)の修正

--- db/schema.rb
+++ db/schema.rb
@@ -20,13 +20,6 @@ ActiveRecord::Schema.define(version: 2023_07_02_053632) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.string "postal_code", null: false
-    t.string "address", null: false
-    t.string "telephone_number", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
@@ -43,6 +36,13 @@ ActiveRecord::Schema.define(version: 2023_07_02_053632) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "telephone_number", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
