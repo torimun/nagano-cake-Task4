735a17d82701588007ea5e0e9fd5cd7a63a199fc
ToriMun <132326048+torimun@users.noreply.github.com>
2023-07-06 05:13:11 +0000

    [fix]routes.rbをresourcesから、個別のto: as の形へ修正

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -4,10 +4,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-   before_action :configure_permitted_parameters, if: :devise_controller?
+   before_action :configure_permitted_parameters, only: [:create]
 
   def after_sign_in_path_for(resource)
-    #mypageへのpath記載
+    public_homes_top_path
   end
 
 
@@ -66,4 +66,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+    keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,10 +24,34 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   namespace :public do
     get 'homes/top'
     get 'homes/about'
-    resources :orders, only: [:new, :confirm, :done, :create, :index, :show]
-    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
-    resources :customers, only: [:show, :edit, :update, :confirm, :withdraw]
     resources :items, only: [:index, :show]
   end
+
+  namespace :public do
+    get 'customers/mypage', to: 'customers#show'
+    get 'customers/information/edit', to: 'customers#edit'
+    patch 'customers/information', to: 'customers#update'
+    get 'customers/confirm_withdraw', to: 'customers#confirm'
+    patch 'customers/withdraw', to: 'customers#withdraw'
+  end
+
+  namespace :public do
+    get 'cart_items/index'
+    patch 'cart_items/update'
+    delete 'cart_items/:id', to: 'cart_items#destroy'
+    delete 'cart_items/destroy_all', to: 'cart_items#destroy_all'
+    post 'cart_items/create'
+  end
+
+  namespace :public do
+    get 'orders/new'
+    post 'orders/confirm', to: 'orders#confirm'
+    get 'orders/done', to: 'orders#done'
+    post 'orders/create'
+    get 'orders/index'
+    get 'orders/show'
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
