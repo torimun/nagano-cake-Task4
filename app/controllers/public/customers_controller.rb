979a1a7a5338819247782e058cebde8f0dbd75e4
ToriMun <132326048+torimun@users.noreply.github.com>
2023-07-08 09:23:04 +0000

    [Add]@customerの追加による会員側のshowページに登録情報が映るように追加（<%= render public/shared/error_messages, rescue: rescue %><% end %>はエラーになるが念のため確保）

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,10 @@
 class Public::CustomersController < ApplicationController
   def show
-
+    @customer=current_customer
   end
 
   def edit
-
+    @customer=current_customer
   end
 
   def update
