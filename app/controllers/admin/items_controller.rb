9222d711949954902800fb66d360bb76b39bb298
ToriMun <132326048+torimun@users.noreply.github.com>
2023-07-10 09:09:25 +0000

    [Add]商品新規登録画面と一覧画面でのeach文を使って作られた分だけ表示できるように作成。次はページネーションの設定

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,20 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
@@ -16,4 +22,12 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:imgae, :name, :introduction, :price)
+  end
+
 end
