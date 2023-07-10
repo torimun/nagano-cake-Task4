27d290dbb089d8d35df7d91858a1104f72ae06b7
ToriMun <132326048+torimun@users.noreply.github.com>
2023-07-10 05:46:36 +0000

    [fix]退会処理関連の記述をsign_upの所からsign_inの場所（正しい場所）に修正

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -3,8 +3,6 @@
 class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
-  before_action :customer_state, only: [:create]
   before_action :configure_permitted_parameters, only: [:create]
 
   # GET /resource/sign_up
@@ -64,17 +62,6 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # end
 
   private
-
-  def customer_state
-    # 【処理内容1】 入力されたemailからアカウントを1件取得
-    @customer = Customer.find_by(email: params[:customer][:email])
-    # アカウントを取得できなかった場合、このメソッドを終了する
-    return if !@customer
-    # 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかつ、is_deletedの値がtrueであるか
-    if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
-        redirect_to new_customer_registration_path
-    end
-  end
 
   def after_sign_in_path_for(resource)
     public_customers_mypage_path
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
   before_action :configure_permitted_parameters
 
   def after_sign_in_path_for(resource)
@@ -35,6 +36,18 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   private
+
+  def customer_state
+    # 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    # アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    # 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかつ、is_deletedの値がtrueであるか
+    if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
+        redirect_to new_customer_registration_path
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in,
     keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
