6c13bca3179cd37685c6b1dd3e3958df11123def
ToriMun <132326048+torimun@users.noreply.github.com>
2023-07-12 08:46:26 +0000

    [Add]会員側のitem,controllerの記載追加、item/indexに大まかな記載の追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,9 @@
 class Public::ItemsController < ApplicationController
   def index
-
+    @items = Item.all.page(params[:page]).per(4)
   end
 
   def show
-
+    @item = Item.find(params[:id])
   end
 end
