02440c28f439999d40835bf006b9d8e037233d51
ToriMun <132326048+torimun@users.noreply.github.com>
2023-07-12 10:24:29 +0000

    [Add]会員側の商品詳細ページにて税込み価格での表示を実装

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,6 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all.page(params[:page]).per(4)
+    @items = Item.all.page(params[:page]).per(8)
   end
 
   def show
