b28e051b4c417a5e0e286767e7b040fa6d5d2b94
ToriMun <132326048+torimun@users.noreply.github.com>
2023-07-13 05:28:54 +0000

    [Add]管理者側、会員詳細画面作成完了、会員情報編集画面は大枠の作成完了

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,15 +8,19 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customers_params)
+    redirect_to admin_customer_path(@cusomer.id)
   end
 
   private
 
   def customers_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
   end
 
 end
