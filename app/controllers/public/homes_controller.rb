c87eb8e223c6a8df759dd7a1f6959da878fe7dca
ToriMun <132326048+torimun@users.noreply.github.com>
2023-07-23 07:43:25 +0000

    [fix]publicのhomes_topページの修正（商品の表示、商品の詳細ページへのリンクの作成）

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,15 @@
 class Public::HomesController < ApplicationController
   def top
-
+    @items = Item.all
   end
 
   def about
 
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :price)
   end
 end
